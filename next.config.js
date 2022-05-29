e77ee104241eb91367334589fc8c8fa1c5028f9c
Roman Sterpov <hisper4@gmail.com>
2022-05-29 15:44:00 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 module.exports = {
+  reactStrictMode: true,
   images: {
     domains: [
       // "[yourapp].wpengine.com" (Update this to be your Wordpress application name in order to load images connected to your posts)
-      'secure.gravatar.com',
+      '1.gravatar.com',
+      'web.loc'
     ],
   },
 }
